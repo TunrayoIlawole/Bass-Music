526213f1d6e93334ac3fb1fef75f77db9591b99e
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2024-10-27 12:06:16 +0100

    update database

--- db/migrate/20241027105938_create_tracks.rb
+++ db/migrate/20241027105938_create_tracks.rb
@@ -2,6 +2,9 @@ class CreateTracks < ActiveRecord::Migration[7.0]
   def change
     create_table :tracks do |t|
 
+      t.string :name
+      t.string :minutes
+      t.references :album
       t.timestamps
     end
   end
